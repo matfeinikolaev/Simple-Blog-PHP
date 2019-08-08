c20c102c2f93b3dceb4d94fa71a5fca4a87c4cf5
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-08 17:53:09 +0300

    Update DataBaseEdit.php

--- MyBlog/database/DataBaseEdit.php
+++ MyBlog/database/DataBaseEdit.php
@@ -3,28 +3,31 @@ class DataBaseEdit
 {
 	protected $pdo;
 
-	public function __construct(PDO $pdo)
+	public function __construct (PDO $pdo)
 	{
 		$this->pdo = $pdo;
 	}
 
-	public function selectAll($table)
+	public function selectAll ($table)
 	{
-		$statement = $this->pdo->prepare("select * from {$table}");
-		$statement->execute();
-		return $statement->fetchAll(PDO::FETCH_CLASS);
+		$statement = $this->pdo->prepare ("select * from {$table}");
+		$statement->execute ();
+		return $statement->fetchAll (PDO::FETCH_CLASS);
 	}
 
-	public function insert($table, $parameters)
-	{	
-		$sql = sprintf('insert into %s (%s) values (%s)', 
-						$table,
-						implode(',', array_keys($parameters)), 
-						':'.implode(', :', array_keys($parameters)));
-		try{
-			$statement=$this->pdo->prepare($sql);
-			$statement->execute($parameters);
-		} catch(Exception $e){die($e->getMessage());}
+	public function insert ($table, $parameters)
+	{
+		$sql = sprintf (
+            'insert into %s (%s) values (%s)',
+			$table,
+			implode (',', array_keys($parameters)),
+			':'.implode (', :', array_keys($parameters))
+        );
+		try {
+			$statement=$this->pdo->prepare ($sql);
+			$statement->execute ($parameters);
+		} catch (Exception $e){
+            die ($e->getMessage ());
+        }
 	}
-
 }
