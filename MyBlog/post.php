e00f16b959db9c65a547962a85abb86d3e6e8738
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-08 16:50:15 +0300

    Update post.php

--- MyBlog/post.php
+++ MyBlog/post.php
@@ -1,8 +1,10 @@
 <?php
 require 'index.php';
 
-$DBedit->insert('Blog',['title' => $_POST['title'],
-						'text' => $_POST['text'],
-						'datetime'=>date("Y-m-d H:i:s")]);
+$DBedit->insert(
+    'Blog',['title' => $_POST['title'],
+    'text' => $_POST['text'],
+    'datetime'=>date("Y-m-d H:i:s")]
+);
 
 header('Location: /');
