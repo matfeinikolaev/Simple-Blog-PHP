bf5a8d6427be710c96563729ae2b2174849cca7d
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-08 16:47:33 +0300

    Update index.view.php

--- MyBlog/index.view.php
+++ MyBlog/index.view.php
@@ -1,10 +1,10 @@
 <?php require 'partials/header.php' ?>
 <?php require 'partials/form.php' ?>
-	<?php foreach ($posts as $post): ?>
-		<h3 class='title'><?= $post->title; ?></h3>
-		<br>
-		<div class='text'><?= $post->text; ?></div>
-		<div class='datetime'><?= $post->datetime; ?></div>
-	<?php endforeach; ?>
+<?php foreach ($posts as $post): ?>
+    <h3 class='title'><?= $post->title; ?></h3>
+    <br>
+    <div class='text'><?= $post->text; ?></div>
+    <div class='datetime'><?= $post->datetime; ?></div>
+<?php endforeach; ?>
 </body>
 </html>
