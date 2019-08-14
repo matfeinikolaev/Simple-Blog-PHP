82587caef2fa887d302879ad6c1fc6d68eedded7
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-14 14:40:11 +0300

    Update form.php

--- MyBlog/partials/form.php
+++ MyBlog/partials/form.php
@@ -1,6 +1,8 @@
-<h2>Jot something down</h2>
-<form method="POST" action="post.php">
-<label for='title'>Title: </label><input type="text" name="title" id="title"><br>
-<input type="text" name="text" placeholder="Type your text in here" id="text"><br>
-<input type="submit" name="submit" id="submit">
+<h2 class="h3 mb-3 font-weight-normal">Jot something down</h2>
+<form method="POST" action="/post">
+  <label class="sr-only" for='title'>Title</label>
+  <input class="form-control" id="title" name="title" type="text" placeholder="The Title"><br>
+  <label class="sr-only" for="text">Text</label>
+  <input class="form-control" id="text" name="text" type="text" placeholder="Type your text in here"><br>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Post</button><br>
 </form>
