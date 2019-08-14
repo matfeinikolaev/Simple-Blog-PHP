a7034ca560a7348bcfdf341a6bd179cc3b630de5
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-14 14:39:12 +0300

    Update Connection.php

--- MyBlog/database/Connection.php
+++ MyBlog/database/Connection.php
@@ -5,11 +5,11 @@ class Connection
     {
         try {
             return new PDO(
-				            $config['connection'].';dbname='.$config['name'], 
-                            $config['user'], 
-                            $config['password'], 
-                            $config['options']
-						);
-		} catch (PDOException $e) {die($e->getMessage());}
-	}
+                $config['connection'].';dbname='.$config['name'], 
+                $config['user'], 
+                $config['password'], 
+                $config['options']
+            );
+        } catch (PDOException $e) {die($e->getMessage());}
+    }
 }
