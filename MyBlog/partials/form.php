6109111700fe73666488c586db9c2abb9ca71824
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-14 15:29:55 +0300

    Delete form.php

--- MyBlog/partials/form.php
+++
@@ -1,8 +0,0 @@
-<h2 class="h3 mb-3 font-weight-normal">Jot something down</h2>
-<form method="POST" action="/post">
-  <label class="sr-only" for='title'>Title</label>
-  <input class="form-control" id="title" name="title" type="text" placeholder="The Title"><br>
-  <label class="sr-only" for="text">Text</label>
-  <input class="form-control" id="text" name="text" type="text" placeholder="Type your text in here"><br>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Post</button><br>
-</form>
