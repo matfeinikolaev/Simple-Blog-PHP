5934d388ffbb46824f74c649c6bec3f84f348312
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-14 14:41:25 +0300

    Update header.php

--- MyBlog/partials/header.php
+++ MyBlog/partials/header.php
@@ -1,8 +1,11 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-	<title>My simple blog</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
+  <title>My simple blog</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="src/bootstrap.min.css">
+  <!-- <link rel="stylesheet" type="text/css" href="src/style.css"> -->
 </head>
 <body>
