ae4849b0bb78ea545bcce8d7f98bc28cc6876864
matfeinikolaev <45239725+matfeinikolaev@users.noreply.github.com>
2019-08-29 21:43:47 +0300

    Update entry-point.php

--- MyBlog/src/signup/entry-point.php
+++ MyBlog/src/signup/entry-point.php
@@ -1,9 +1,9 @@
 <?php require 'src/partials/header.php' ?>
-	<form method="POST" action="/signUp" class='form-signin'>
+	<form method="POST" action="/sign-up" class='form-signin'>
 		<button  class="btn btn-lg btn-primary btn-block">Sign Up</button>
 	</form>
 	<br>
-	<form method="POST" action="/logIn" class='form-signin'>
+	<form method="POST" action="/log-in" class='form-signin'>
 		<button class="btn btn-lg btn-primary btn-block">Log In</button>
 	</form>
 </body>
